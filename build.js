2d1060238a40bc8570a087cbc27201f4ad78c14f
1eqj03hqk <137458892+1eqj03hqk@users.noreply.github.com>
2025-07-10 00:35:09 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 const { execSync } = require('child_process');
 
-console.log(process.env)
-console.log(process.env.REBUILD_SPEC)
-
 if (process.env.REBUILD_SPEC === 'true') {
   console.log('Running openapi-preprocessor...');
   execSync('node src/scripts/openapi-preprocessor.js', { stdio: 'inherit' });
